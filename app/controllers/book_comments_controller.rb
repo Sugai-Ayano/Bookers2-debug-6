4a81e714eceec613ff3ba41cac13379e6bf996d7
Sugai-Ayano <b3w41067a@gmail.com>
2021-01-07 11:58:10 +0000

    [Fix]課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,19 +6,16 @@ class BookCommentsController < ApplicationController
 		@book_comment = BookComment.new(book_comment_params)
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
-		if @book_comment.save
-  		redirect_to book_path(@book.id)
-		else
-		  redirect_back(fallback_location: root_path)
-		end
+		 @book_comment.save
+		# else
+		#   redirect_back(fallback_location: root_path)
+		# end
 	end
 
 	def destroy
 		BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-		# @book = Book.find(params[:id])
-  # 		book_comment = @book.book_comments.find(params[:id])
-		# book_comment.destroy
-    	redirect_back(fallback_location: root_path)
+		@book = Book.find(params[:book_id])
+   	
 	end
 
 	private
